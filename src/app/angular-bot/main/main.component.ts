101083566e183963915b93005891be482f044079
Ali Raja <muhammadaliraja121@gmail.com>
2021-10-11 23:41:54 +0500

    home page

--- src/app/angular-bot/main/main.component.ts
+++ src/app/angular-bot/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Message, ChatService } from '../chat.service';
+
 
 @Component({
   selector: 'app-main',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
-
+  messages: Message[] = [];
+  value: any;
+  toggleChat = false;
+  constructor(public chatService: ChatService) { }
+  
   ngOnInit(): void {
+    this.chatService.conversation.subscribe((val) => {
+      this.messages = this.messages.concat(val);
+    });
   }
 
+  sendMessage() {
+    if (this.value) {
+      this.chatService.getBotAnswer(this.value);
+      this.value = '';
+    }  
+  }
+
+  toggleChatBtn () {
+    this.toggleChat = !this.toggleChat;
+  }
 }
